a222195b46d0f78de09e2a35b740a3fd5e858a77
Kastor438 <Kastor438@hotmail.com>
2023-03-05 22:54:36 -0400

    Fixed fullscreen to be active from launch of app and continue to be in levels.

--- app/src/main/java/com/example/csci4176_groupproject/Level1Activity.kt
+++ app/src/main/java/com/example/csci4176_groupproject/Level1Activity.kt
@@ -3,27 +3,44 @@ package com.example.csci4176_groupproject
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
+import android.view.View
+import android.view.WindowInsets
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
+import com.example.csci4176_groupproject.databinding.ActivityLevel1Binding
 import kotlin.math.abs
 
 
 class Level1Activity : AppCompatActivity() {
     private lateinit var detector: GestureDetectorCompat
+
+    private lateinit var binding: ActivityLevel1Binding
+    private lateinit var fullscreenContent: TextView
+
+    private var isFullscreen: Boolean = true
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_level_1)
+        binding = ActivityLevel1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.hide()
+
+        isFullscreen = true
+
+        // Set up the user interaction to manually show or hide the system UI.
+        fullscreenContent = binding.Level1FullscreenContent
+
+//        setContentView(R.layout.activity_level_1)
 
         detector = GestureDetectorCompat(this, GestureListener())
 
@@ -33,10 +50,16 @@ class Level1Activity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val groundTile1 = findViewById<ImageView>(R.id.GroundTile1)
+        hide()
+    }
 
-        groundTile1.setOnClickListener {
-            groundTile1.setColorFilter(Color.argb(80, 225, 68, 19))
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        isFullscreen = false
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            fullscreenContent.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
         }
     }
 

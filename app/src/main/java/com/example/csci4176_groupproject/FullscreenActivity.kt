9c0b06e1bebe9528c2466cc3b59ddcffe368216f
PatnSuraj <suraj.patnaikuni@dal.ca>
2023-03-26 16:58:07 -0300

    Main menu functionalities

--- app/src/main/java/com/example/csci4176_groupproject/FullscreenActivity.kt
+++ app/src/main/java/com/example/csci4176_groupproject/FullscreenActivity.kt
@@ -6,13 +6,11 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowInsets
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import com.example.csci4176_groupproject.databinding.ActivityFullscreenBinding
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,8 +18,16 @@ import androidx.appcompat.app.AppCompatActivity
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : BaseActivity() {
+class FullscreenActivity : BaseActivity(), settingsDialogCallback {
     private lateinit var binding: ActivityFullscreenBinding
+
+    override fun settingsDialogCallback(settings: settingsData){
+        val changes = settings.changes
+        if (changes["playerIcon"]!!){
+            Log.d("seting change", "changing player icon setting")
+        }
+
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +47,17 @@ class FullscreenActivity : BaseActivity() {
         val testLevelButton = findViewById<Button>(R.id.ToLevelTest);
 
         testLevelButton.setOnClickListener {
+            val intent = Intent(this, LevelSelect::class.java)
+            startActivity(intent)
+        }
+
+        val settingsButton = findViewById<ImageButton>(R.id.SettingsButton)
+        settingsButton.setOnClickListener {
+            settingsDialog(context = this).showSettings(this)
+        }
+
+        val menuButtonAction = findViewById<ImageButton>(R.id.menuButton)
+        menuButtonAction.setOnClickListener {
             val intent = Intent(this, LevelSelect::class.java)
             startActivity(intent)
         }

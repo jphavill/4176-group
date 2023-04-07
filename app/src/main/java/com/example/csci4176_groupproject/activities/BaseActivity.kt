a9330973880c7031d7092e16bdb7b15fe490c615
jphavill <jphavill@gmail.com>
2023-04-07 20:30:31 -0300

    comments for BaseActivity

--- app/src/main/java/com/example/csci4176_groupproject/activities/BaseActivity.kt
+++ app/src/main/java/com/example/csci4176_groupproject/activities/BaseActivity.kt
@@ -11,12 +11,14 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.csci4176_groupproject.R
 import com.example.csci4176_groupproject.data.CosmeticList
+import com.example.csci4176_groupproject.data.LevelActivities
 import com.example.csci4176_groupproject.fragments.TopBarFragment
 import com.example.csci4176_groupproject.models.Level
 import com.example.csci4176_groupproject.viewModels.SettingsViewModel
 import com.google.gson.Gson
 
 abstract class BaseActivity : AppCompatActivity() {
+    // All activies in the app inherit from this
     var isFullscreen: Boolean = true
     lateinit var fullscreenContent: FrameLayout
     lateinit var settingPrefs: SharedPreferences
@@ -24,11 +26,15 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // hide status bar
         hideAndroidUI()
 
+        // persistent storage shared by the entire application
         settingPrefs = this.applicationContext.getSharedPreferences("settingsPrefs", 0)
+        // haptics are enabled by default, or set to the setting stored in persistent storage
         updateHaptics(settingPrefs.getBoolean("haptics", true))
 
+        // observors to update settings, will be triggered by the TopBar fragment
         settingsViewModel.haptics.observe(this) { state ->
             updateHaptics(state)
         }
@@ -41,20 +47,26 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     private fun updateHaptics(state: Boolean) {
+        // controls if haptics are enabled for the entire activity with one setting
         window.decorView.rootView.isHapticFeedbackEnabled = state
     }
 
     fun addTopBar(title: String, backActivity: String) {
+        // adds the TopBar fragment that holds the back button, title and settings button
         val topBar = TopBarFragment()
         val args = Bundle()
         args.putString("title", title)
+        // determines what activity the back button will open
         args.putString("backActivity", backActivity)
         topBar.arguments = args
         supportFragmentManager.beginTransaction().add(R.id.topBar, topBar).commit()
     }
 
     private fun hideAndroidUI() {
-        // Hide UI first
+        // Hides the android status bar at the top of the screen
+        // without doing this, relativeLayouts are offset by the height of the status bar
+        // this would make the player character appear in the wrong place
+        // since status bars can be different heights, the easiest way to deal with this is to hide it
         supportActionBar?.hide()
         isFullscreen = false
 
@@ -65,6 +77,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
 
     fun getViewsByTag(root: ViewGroup, tag: String): ArrayList<View> {
+        // gets all views in an xml layout that have a certian tag. Order IS maintained
         val views: ArrayList<View> = ArrayList()
         val childCount = root.childCount
 
@@ -82,6 +95,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        // enable fullscreen mode
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
         } else {
@@ -90,27 +104,32 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     private fun resetLevels() {
-        for (id in 1 until 11) {
-            // the last level on the first page, and all subsequent levels, are locked by default
+        // starts at 1 because levels are id'd starting at 1 so level 1 has an id of level1 instead of level0
+        for (id in 1  .. LevelActivities().levels.size) {
+            // all levels after level 5 are locked by default
             val tempLevel = Level(id = id, locked = id > 5)
+            // overwrite each level object in persistent storage to reset them
             val gson = Gson()
             val editor: SharedPreferences.Editor = settingPrefs.edit()
             tempLevel.starsEarned = 0
             tempLevel.time = -1
             tempLevel.tried = false
             editor.putString(String.format("level%d", id), gson.toJson(tempLevel))
+            // reset the number of stars the user has earned to 0
             editor.putInt("stars", 0)
             editor.apply()
         }
     }
 
     private fun resetStore() {
+        // overwrite each cosmetic object in persistent storage to reset them
         val gson = Gson()
         val editor: SharedPreferences.Editor = settingPrefs.edit()
         for (id in 0 until CosmeticList().itemList.size) {
             val tempCosmetic = CosmeticList().itemList[id]
             editor.putString(String.format("cosmetic%d", id), gson.toJson(tempCosmetic))
         }
+        // reset the playerSkin to the default skin, since that will be the only one unlocked
         editor.putInt("playerSkin", CosmeticList().itemList[0].img)
         editor.apply()
     }

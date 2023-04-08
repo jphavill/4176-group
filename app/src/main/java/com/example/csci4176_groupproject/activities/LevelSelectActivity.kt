fba929732084c61182106e23b90b2cd1561b5fa4
jphavill <jphavill@gmail.com>
2023-04-07 21:28:31 -0300

    add comments to levelselectactivity

--- app/src/main/java/com/example/csci4176_groupproject/activities/LevelSelectActivity.kt
+++ app/src/main/java/com/example/csci4176_groupproject/activities/LevelSelectActivity.kt
@@ -38,9 +38,13 @@ class LevelSelectActivity : BaseActivity(), BuyDialogCallback {
 
         super.addTopBar("Level Select", "MainMenuActivity")
 
+        // observor to update the starCount when a user purchases a level for example
         starCount.starCount.observe(this) {
             updateStars()
         }
+        // this is in addition to the obeservor in BaseActivity that this Activity also inherits
+        // this is because the level buttons must live update to reflect this, without requiring
+        // the activity to be reloaded
         settingsViewModel.resetLevels.observe(this) {
             updateButtons()
         }
@@ -51,12 +55,18 @@ class LevelSelectActivity : BaseActivity(), BuyDialogCallback {
         updateNavButtons()
         val fullScreenView: ViewGroup = findViewById(R.id.levelSelectFullscreenContent)
         val levelButtons = getViewsByTag(fullScreenView, "levelButton")
+        // button index is used to determine what level each button links to
+        // therefore the buttons on page 0 need different index's then page 1 etc
         var buttonIndex = pageNumber * perPage
         for (b in levelButtons) {
+            // for each button on the page, set the corresponding fragment
             val frag = LevelButtonFragment()
             val args = Bundle()
             args.putInt("buttonIndex", buttonIndex)
             frag.arguments = args
+            // if the buttons are being updated after the page has initially been created,
+            // ie when a user resets the levels, then replace the existing fragments instead of
+            // adding new ones
             if (replace) {
                 supportFragmentManager.beginTransaction().replace(b.id, frag).commit()
             } else {
@@ -64,6 +74,7 @@ class LevelSelectActivity : BaseActivity(), BuyDialogCallback {
             }
             buttonIndex += 1
         }
+        // tracks that fragments have now been added and should therefore be replaced in the future
         replace = true
     }
 
@@ -75,6 +86,8 @@ class LevelSelectActivity : BaseActivity(), BuyDialogCallback {
     private fun updateNavButtons() {
         val levels = LevelActivities().levels
         val nextView = findViewById<ImageButton>(R.id.levelsNextButton)
+        // if the pageNumber is less than the total number of levels divided by the number of levels
+        // per page, then that means there are more pages. Therefore show the next page button
         if (pageNumber < kotlin.math.floor(levels.size / perPage.toDouble())) {
             nextView.setOnClickListener {
                 pageNumber++
@@ -85,6 +98,7 @@ class LevelSelectActivity : BaseActivity(), BuyDialogCallback {
             nextView.visibility = View.INVISIBLE
         }
         val backView = findViewById<ImageButton>(R.id.levelsBackButton)
+        // if not on the first page then show the previous page button
         if (pageNumber > 0) {
             backView.setOnClickListener {
                 pageNumber--
